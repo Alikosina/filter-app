193fa821e3ae9b49b06ceb5b73ebc33e0a5fef97
Ali Sloyan <alisloyan@MacBook-Pro-Ali.local>
2019-08-02 21:19:50 +0300

    Provided app store

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
 
-const AppContainer = () => <div />;
+const AppContainer = () => <div>Hello Filter-App!</div>;
 
 export default AppContainer;
---
+++ src/store/orders/ordersReducer.ts
@@ -0,0 +1,8 @@
+const initialState: any = {
+  loading: false,
+  data: []
+};
+
+export default (state = initialState, action: any) => {
+  return state;
+};
